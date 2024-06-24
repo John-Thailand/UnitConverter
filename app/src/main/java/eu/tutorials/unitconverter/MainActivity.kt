5c16e3dd20a453936365c6d948d9fbd57416ad7b
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-24 18:50:09 +0900

    Implementing state to outlinedTextfield

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -22,6 +22,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +51,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(0.01) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -54,7 +66,13 @@ fun UnitConverter() {
     ) {
         Text("Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+            inputValue = it
+            },
+            label = {Text("Enter Value")}
+        )
         Spacer(modifier = Modifier.height(16.dp))
         Row {
             Box {
@@ -62,7 +80,7 @@ fun UnitConverter() {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
                     DropdownMenuItem(
                         text = { Text("Centimeters" ) },
                         onClick = { /*TODO*/ }
@@ -87,7 +105,7 @@ fun UnitConverter() {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
                     DropdownMenuItem(
                         text = { Text("Centimeters" ) },
                         onClick = { /*TODO*/ }
